682c9ec9cab0fb95e8d266edd44b3e7fc3ed5cdb
Martin Vedani <martin.vedani@gmail.com>
2022-05-26 21:04:55 +0200

    pipe '-vvvv' into subprocess.run

--- pre_commit_hooks/ansible_inventory_wrapper.py
+++ pre_commit_hooks/ansible_inventory_wrapper.py
@@ -2,6 +2,7 @@ import subprocess
 import argparse
 from typing import Optional, Sequence
 
+
 def main(argv: Optional[Sequence[str]] = None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*')
@@ -10,14 +11,16 @@ def main(argv: Optional[Sequence[str]] = None):
 
     for filename in args.filenames:
         cp = subprocess.run(
-            ['ansible-inventory','--list','-i',filename],
+            ['ansible-inventory', '--list', '-i', filename, '-vvvv'],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
         if(cp.stderr):
-            print(f"ansible-inventory failed on file {filename} with following output:")
+            print(
+                f"ansible-inventory failed on file {filename} with following output:")
             print(cp.stderr.decode())
             result = 1
     exit(result)
+
 
 if __name__ == '__main__':
     exit(main())
